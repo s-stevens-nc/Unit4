df20548380ad10311e2a24cdc2ca946d3e1770db
Jakub Dulas <j-dulas@student.newbury-college.ac.uk>
2021-03-19 10:21:37 +0000

    Put in 2  tickboxes

--- Unit4/Form1.Designer.cs
+++ Unit4/Form1.Designer.cs
@@ -31,8 +31,9 @@ namespace Unit4
 		{
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
             // treeView1
@@ -45,36 +46,53 @@ namespace Unit4
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(99, 60);
+            this.textBox1.Location = new System.Drawing.Point(89, 181);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(188, 20);
             this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "Forename";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(99, 160);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(188, 20);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.Text = "Surname";
+            this.textBox1.Text = "Name";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // checkedListBox1
             // 
             this.checkedListBox1.AccessibleName = "Team ";
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(343, 221);
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Individual"});
+            this.checkedListBox1.Location = new System.Drawing.Point(89, 243);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(588, 214);
+            this.checkedListBox1.Size = new System.Drawing.Size(81, 34);
             this.checkedListBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(237, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.AccessibleName = "Team ";
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "One event"});
+            this.checkedListBox2.Location = new System.Drawing.Point(237, 243);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(81, 34);
+            this.checkedListBox2.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1313, 824);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.checkedListBox1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.treeView1);
             this.Name = "Form1";
@@ -88,8 +106,9 @@ namespace Unit4
 
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
     }
 }
 
--- Unit4/Form1.cs
+++ Unit4/Form1.cs
@@ -22,5 +22,15 @@ namespace Unit4
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
