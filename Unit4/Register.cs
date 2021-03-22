09a01e56a947a5f12e74fd4f2ef52f74e45ae82f
Soul Stevens <s-stevens@student.newbury-college.ac.uk>
2021-03-22 10:25:25 +0000

    Add methods to register teams and events

--- Unit4/Register.cs
+++ Unit4/Register.cs
@@ -15,26 +15,112 @@ namespace Unit4
     {
 
         // Even though indivusuals and teams are both team structures, we are keeing them in seperate lists
-        List<Team> Individuals;
+        static List<Team> Individuals;
         List<Team> Teams;
         List<Event> Events;
 
-        public struct Team
+        public class Team
         {
-            string Name;
-            bool IsIndividual;
-            int Points;
-            int ID;
-            bool SingleEvent;
+            public string Name;
+            public bool IsIndividual;
+            public int Points;
+            public int ID;
+            public bool SingleEvent;
         }
 
-        public struct Event
+        public class Event
         {
-            string Name;
-            bool IsIndividual;
-            int ID;
-            bool Done;
-            List<int> ParticipantsIDs;
+            public string Name;
+            public bool IsIndividual;
+            public int ID;
+            public bool Done;
+            public List<int> ParticipantsIDs;
+        }
+
+        // Method to create a new team
+        public void RegisterTeam (string name, bool isIndividual, bool singleEvent)
+        {
+            Team _team = new Team(); // Since event NEEDS an underscore in Method ReisterEvent ive added an underscore here for consincy
+            _team.Name = name;
+            _team.SingleEvent = singleEvent;
+            _team.IsIndividual = isIndividual;
+
+            _team.Points = 0;
+
+            _team.ID = Teams.Count + Individuals.Count;
+
+            if (isIndividual && Individuals.Count < 20)
+            {
+                Individuals.Add(_team);
+            } else if (!isIndividual)
+            {
+                Teams.Add(_team);
+            }
+        }
+
+        // Method to create a new event
+        public void RegisterEvent(string name, bool isIndividual)
+        {
+            Event _event = new Event (); // event has an underscore since event is a reserved varaible in C#... GRRR
+            _event.Name = name;
+            _event.IsIndividual = isIndividual;
+            _event.ID = Events.Count;
+            _event.ParticipantsIDs = new List<int>();
+
+            Events.Add(_event);
+        }
+
+        // Method to enroll a team into an event if elligble
+        public int EnrollTeam (int eventID, Team team)
+        {
+
+            // Check team is not in more than 1 or 5 evets
+            int count = 0;
+            foreach (Event e in Events)
+            {
+                for (int i = 0; i<e.ParticipantsIDs.Count; i++)
+                {
+                    if (e.ParticipantsIDs[i] == team.ID) count++;
+                }
+            }
+            // Return if team is trying to enter more events than they should be
+            if (team.SingleEvent && count > 1) return -1;
+            else if (!team.SingleEvent && count > 5) return -1;
+
+            Events[eventID].ParticipantsIDs.Add(team.ID);
+            return 0;
+        }
+
+        public void RegsiterResult(List<int> winners, int eventID, bool isIndividual)
+        {
+            for (int i=0; i<winners.Count; i++)
+            {
+                // Get index of team or person
+                int index = 0;
+                if (isIndividual)
+                {
+                    for (int j=0; j<Individuals.Count; j++)
+                    {
+                        if (Individuals[i].ID == winners[i])
+                        {
+                            index = j;
+                            break;
+                        }
+                    }
+                    Individuals[index].Points += (winners.Count - i);
+                } else
+                {
+                    for (int j = 0; j < Teams.Count; j++)
+                    {
+                        if (Teams[i].ID == winners[i])
+                        {
+                            index = j;
+                            break;
+                        }
+                    }
+                    Teams[index].Points += (winners.Count - i);
+                }
+            }
         }
     }
 }
